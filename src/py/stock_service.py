c3559c51e16a64eeeb88206a493a608b004179b2
henryhkyim <40025988+henryhkyim@users.noreply.github.com>
2018-07-26 23:30:29 +0800

    update stock_service and add .gitignore

--- src/py/stock_service.py
+++ src/py/stock_service.py
@@ -1,11 +1,11 @@
 from flask import Flask
 app = Flask(__name__)
 
-supported_stock_code = ["5", "6", "700", "7336"]
+supported_symbol_list = ["5", "6", "700", "7336"]
 
-def get_stock_by_code(stock_code):
-	return "{stock_code: " + stock_code + ", stock_price: " + str(73.2) + "}" if stock_code in supported_stock_code else "{}"
+def get_stock_by_symbol(symbol):
+	return "{symbol: " + symbol + ", stock_price: " + str(73.2) + "}" if symbol in supported_symbol_list else "{}"
 
-@app.route("/stock-api/stock_code/<stock_code>")
-def get_stock(stock_code):
-  return get_stock_by_code(stock_code)
+@app.route("/stock-service/symbol/<symbol>")
+def get_stock(symbol):
+  return get_stock_by_symbol(symbol)
