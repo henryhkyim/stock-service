a928e1f174bca2e1c6b5a86d7fc792ca12698377
henryhkyim <40025988+henryhkyim@users.noreply.github.com>
2018-08-03 20:53:18 +0800

    change to use map dataytype

--- src/py/stock_service.py
+++ src/py/stock_service.py
@@ -1,10 +1,16 @@
+import json
 from flask import Flask
 app = Flask(__name__)
 
-supported_symbol_list = ["5", "6", "700", "7336"]
+symbol_map = {
+	"00005.HK": {"desc": "HSBC HOLDINGS", "price": 74.55, "prev_close": 74.6},
+	"00006.HK": {"desc": "POWER ASSETS", "price": 55.4, "prev_close": 55.95},
+	"00700.HK": {"desc": "TENCENT", "price": 356.4, "prev_close": 367.2},
+	"07336.HK": {"desc": "FI MR HS", "price": 5.73, "prev_close": 5.71}
+}
 
 def get_stock_by_symbol(symbol):
-	return "{symbol: " + symbol + ", stock_price: " + str(73.2) + "}" if symbol in supported_symbol_list else "{}"
+	return json.dumps(symbol_map.get(symbol) if symbol in symbol_map else {}, indent=2, separators=(", ", ": "))
 
 @app.route("/stock-service/symbol/<symbol>")
 def get_stock(symbol):
